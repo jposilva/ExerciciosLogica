4708fc04c78c171417433fba24cb59e2ab56c698
João Paulo <jposilva@msn.com>
2023-07-25 20:50:33 -0300

    Exercicios de 1 a 3

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const prompt=require("prompt-sync")();
-console.log ("Hello World");
+
+function Exercicio1(){
+    console.log ("Hello World");
+}
+
+//Exercicio1();
+
+function Exercicio2(){
+   const nome = prompt ("Digite seu nome:");
+   console.log (`O nome digitado foi ${nome}`);
+}
+//Exercicio2();
+
+function Exercicio3(){
+    const num1 = parseInt (prompt ("Digite o primeiro número:"));
+    const num2 = parseInt (prompt ("Digite o segundo número:"));
+    console.log (`A média dos dois números é ${(num1+num2)/2}`)
+}
+Exercicio3()
